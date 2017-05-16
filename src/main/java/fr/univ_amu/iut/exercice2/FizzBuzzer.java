656db2d4ca4172736661d63b6481561a97e40efa
Antonin Gaboriau <antonin.gaboriau@etu.univ-amu.fr>
2017-05-16 11:51:30 +0200

    Validations de l'exercice 2

--- src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
+++ src/main/java/fr/univ_amu/iut/exercice2/FizzBuzzer.java
@@ -3,10 +3,21 @@ package fr.univ_amu.iut.exercice2;
 public class FizzBuzzer {
 
     public String computeString(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        if (i % 3 == 0 && i % 5 == 0)
+            return "FizzBuzz";
+        else if (i % 3 == 0)
+            return "Fizz";
+        else if (i % 5 == 0)
+            return "Buzz";
+        else
+            return Integer.toString(i);
     }
 
     public String[] computeList(int i) {
-        throw new RuntimeException("Not yet implemented !");
+        String[] tab = new String[i];
+        for (int j = 0; j < i; ++j) {
+            tab[j] = computeString(j + 1);
+        }
+        return tab;
     }
 }
