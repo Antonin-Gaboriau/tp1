c4a1549243620c045deaf593725f0eb29d0bf539
Antonin Gaboriau <antonin.gaboriau@etu.univ-amu.fr>
2017-05-16 12:16:47 +0200

    Exercice 3 en cours

--- src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
+++ src/main/java/fr/univ_amu/iut/exercice3/ConvertisseurDeNombreRomain.java
@@ -1,7 +1,25 @@
 package fr.univ_amu.iut.exercice3;
 
+import java.util.HashMap;
+
 public class ConvertisseurDeNombreRomain {
     public int enNombreArabe(String i) {
-        throw new RuntimeException("Not yet implemented !");
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int res = 0;
+
+        for (char c : i.toCharArray()) {
+            if
+            res += map.get(c);
+        }
+
+        return res;
     }
 }
